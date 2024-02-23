87ee2977bc57188bf02ae73d5ce2315ea97ab9ec
Dimitri PERREAUX <dimitri.perreaux@gmail.com>
2024-02-23 15:34:18 +0100

    test(webdriver): functional scraping scrpit

--- scraping_selenium.py
+++ scraping_selenium.py
@@ -9,8 +9,21 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 import pandas as pd
 
-driver = webdriver.Chrome('D:/Téléchargements/chromedriver_win32')
-driver.get('https://demowebshop.tricentis.com/')
-web = driver.maximize_window()
-time.sleep(5)
-print("Ouvre URL et Agrandir")
+driver = webdriver.Chrome()
+driver.get('https://demowebshop.tricentis.com/blue-jeans')
+time.sleep(2)
+title = driver.title
+print(title)
+url = driver.current_url
+print(url)
+product_form = driver.find_element(By.ID, "product-details-form")
+print(product_form.get_attribute("action"))
+print(product_form.get_attribute("method"))
+image_jean = driver.find_element(By.ID, "main-product-img-36")
+print(image_jean.get_attribute("src"))
+price = driver.find_element(By.CLASS_NAME, "price-value-36")
+print(price.get_attribute("innerText"))
+stock = driver.find_element(By.CLASS_NAME, "value")
+print(stock.get_attribute("innerText"))
+h1 = driver.find_element(By.TAG_NAME, "h1")
+print(h1.get_attribute("innerText"))
