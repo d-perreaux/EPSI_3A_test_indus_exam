e7e6d026b7c74d58c4f4a2e30c4821e5900da939
Dimitri PERREAUX <dimitri.perreaux@gmail.com>
2024-02-24 07:59:13 +0100

    refactor(scraping_selenium): display result in console

--- tests_selenium/scraping_selenium.py
+++ tests_selenium/scraping_selenium.py
@@ -15,14 +15,26 @@ def get_title(driver):
 def get_current_url(driver):
     return driver.current_url
 
-def get_image_source(driver, image_id):
-    image_element = driver.find_element(By.ID, image_id)
+def get_image_source(driver):
+    image_element = driver.find_element(By.ID, 'main-product-img-36')
     return image_element.get_attribute("src")
 
-def get_element_text(driver, by_method, identifier):
-    element = driver.find_element(by_method, identifier)
+def get_element_article(driver):
+    element = driver.find_element(By.TAG_NAME, 'h1')
     return element.get_attribute("innerText")
 
 def get_product_price(driver):
     price_element = driver.find_element(By.CLASS_NAME, "price-value-36")
     return price_element.get_attribute("innerText")
+
+
+if __name__ == '__main__':
+    driver = initialize_driver()
+    get_page(driver, 'https://demowebshop.tricentis.com/blue-jeans')
+    print("Titre:", get_title(driver))
+    print("URL:", get_current_url(driver))
+    print("Image Source:", get_image_source(driver))
+    print("Prix:", get_product_price(driver))
+    print("Element article:", get_element_article(driver))
+
+    
